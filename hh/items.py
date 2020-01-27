29fe78c9b0df22a14d1cae96a0a46a3f494f52e8
superbe <babarykin@list.ru>
2020-01-27 23:36:52 +0500

    Доработка по обходу.

--- hh/items.py
+++ hh/items.py
@@ -8,21 +8,83 @@
 import scrapy
 from scrapy.loader.processors import MapCompose, TakeFirst
 import re
+import dateutil.parser
 
 regex_html = re.compile(r'(\<(/?[^>]+)>)')
 regex_s = re.compile(r'\s+')
+regex_d = re.compile(r'\d+')
 
 
 def clear_description(value):
     return regex_s.sub(' ', regex_html.sub('', value)).strip()
 
 
+def clear_vacancy(value):
+    return clear_description(value).split('(')[0].strip()
+
+
+def clear_salary(value):
+    return regex_d.findall(clear_description(value).replace(' ', ''))
+
+
+def clear_min_salary(value):
+    return int(clear_salary(value)[0])
+
+
+def clear_max_salary(value):
+    result = clear_salary(value)
+    return int(result[1]) if len(result) == 2 else int(result[0])
+
+
+def clear_city(value):
+    return clear_description(value).split(',')[0].strip()
+
+
+def clear_address(value):
+    result = clear_description(value).split(',')
+    return int(result[1].strip()) if len(result) == 2 else 'Без адреса'
+
+
+def clear_tags(value):
+    return clear_description(value).split(', ')
+
+
+def clear_date(value):
+    return dateutil.parser.parse(value)
+
+
 class HhItem(scrapy.Item):
+    # Идентификатор записи.
     _id = scrapy.Field()
+    # Ссылка на вакансию.
     url = scrapy.Field(output_processor=TakeFirst())
+    # Дата парсинга.
     parse_date = scrapy.Field(output_processor=TakeFirst())
+    # Регион.
+    region = scrapy.Field(output_processor=TakeFirst())
+    # Заголовок вакансии.
     title = scrapy.Field(input_processor=MapCompose(clear_description), output_processor=TakeFirst())
+    # Вакансия.
+    vacancy = scrapy.Field(input_processor=MapCompose(clear_vacancy), output_processor=TakeFirst())
+    # Описание.
     description = scrapy.Field(input_processor=MapCompose(clear_description), output_processor=TakeFirst())
+    # Зарплата.
     salary = scrapy.Field(input_processor=MapCompose(clear_description), output_processor=TakeFirst())
+    # Минимальная величина вакансии.
+    min_salary = scrapy.Field(input_processor=MapCompose(clear_min_salary), output_processor=TakeFirst())
+    # Максимальная величина вакансии.
+    max_salary = scrapy.Field(input_processor=MapCompose(clear_max_salary), output_processor=TakeFirst())
+    # Город.
+    city = scrapy.Field(input_processor=MapCompose(clear_city), output_processor=TakeFirst())
+    # Адрес.
+    address = scrapy.Field(input_processor=MapCompose(clear_address), output_processor=TakeFirst())
+    # Опыт работы.
+    experience = scrapy.Field(input_processor=MapCompose(clear_tags), output_processor=TakeFirst())
+    # Занятость.
+    employment = scrapy.Field(input_processor=MapCompose(clear_tags), output_processor=TakeFirst())
+    # Дата публикации
+    publication_date = scrapy.Field(input_processor=MapCompose(clear_date), output_processor=TakeFirst())
+    # Организация.
     organisation = scrapy.Field(input_processor=MapCompose(clear_description), output_processor=TakeFirst())
+    # Урла организации.
     organisation_url = scrapy.Field(output_processor=TakeFirst())
--- hh/spiders/hhspiser.py
+++ hh/spiders/hhspiser.py
@@ -19,41 +19,54 @@ class HhSpider(scrapy.Spider):
 
         for area in clusters:
             if "&area=" in area:
-                if "area=1&" in area or "area=2&" in area:
-                    yield response.follow(area, callback=self.big_city_parse)
+                region = response.xpath(f'//a[contains(@href, "{area}")]/' +
+                                        'span[contains(@data-qa, "serp__cluster-item-title")]/text()').extract_first()
+                if "&area=1&" in area:
+                    yield response.follow(area, callback=self.big_city_parse, cb_kwargs={'region': region})
+                elif "&area=2&" in area:
+                    yield response.follow(area, callback=self.big_city_parse, cb_kwargs={'region': region})
                 else:
-                    yield response.follow(area, callback=self.city_parse)
+                    yield response.follow(area, callback=self.city_parse, cb_kwargs={'region': region})
 
-    def big_city_parse(self, response: HtmlResponse):
+    def big_city_parse(self, response: HtmlResponse, region):
         by_metro = response.xpath(
             '//ul[contains(@class, "clusters-list")]/li[contains(@class, "clusters-list__item")]/a[contains(@class, ' +
             '"clusters-value")]/@href').extract()
 
         for metro in by_metro:
-            if "$metro=" in metro:
-                yield response.follow(metro, callback=self.city_parse)
+            if "&metro=" in metro:
+                yield response.follow(metro, callback=self.city_parse, cb_kwargs={'region': region})
+            elif "&area=" in metro:
+                yield response.follow(metro, callback=self.city_parse, cb_kwargs={'region': region})
 
-    def city_parse(self, response: HtmlResponse):
+    def city_parse(self, response: HtmlResponse, region):
         next_page = response.xpath(
             '//div[contains(@data-qa, "pager-block")]/a[contains(@data-qa, "pager-next")]/@href').extract_first()
         vacancies = response.xpath(
             '//span[contains(@class, "g-user-content")]/a[contains(@data-qa, "vacancy-serp__vacancy-title")]/@href').extract()
 
         if next_page:
-            yield response.follow(next_page, callback=self.parse)
+            yield response.follow(next_page, callback=self.city_parse, cb_kwargs={'region': region})
 
         for vacancy in vacancies:
-            yield response.follow(vacancy[18:], callback=self.vacancy_parse)
+            yield response.follow(vacancy[18:], callback=self.vacancy_parse, cb_kwargs={'region': region})
 
-    def vacancy_parse(self, response: HtmlResponse):
+    def vacancy_parse(self, response: HtmlResponse, region):
         item = ItemLoader(HhItem(), response)
-
         item.add_value('url', response.url)
+        item.add_value('region', region)
         item.add_xpath('title', '//h1[contains(@data-qa, "vacancy-title")]')
+        item.add_xpath('vacancy', '//h1[contains(@data-qa, "vacancy-title")]')
         item.add_xpath('salary', '//p[contains(@class, "vacancy-salary")]')
+        item.add_xpath('min_salary', '//p[contains(@class, "vacancy-salary")]')
+        item.add_xpath('max_salary', '//p[contains(@class, "vacancy-salary")]')
+        item.add_xpath('city', '//span[contains(@data-qa, "vacancy-view-raw-address")]')
+        item.add_xpath('address', '//span[contains(@data-qa, "vacancy-view-raw-address")]')
+        item.add_xpath('experience', '//span[contains(@data-qa, "vacancy-experience")]')
+        item.add_xpath('employment', '//p[contains(@data-qa, "vacancy-view-employment-mode")]')
+        item.add_xpath('publication_date', '//meta[contains(@itemprop, "datePosted")]/@content')
         item.add_xpath('organisation',
                        '//a[contains(@class, "vacancy-company-name")]/span[contains(@itemprop, "name")]')
         item.add_xpath('organisation_url', '//a[contains(@class, "vacancy-company-name")]/@href')
         item.add_xpath('description', '//div[contains(@data-qa, "vacancy-description")]')
-
         yield item.load_item()
